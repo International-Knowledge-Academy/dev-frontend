1fb4f84c75803ec16318c2a8835e068b406c3234
adnanmadi241 <adnan.madi@codeandcanvas.io>
2026-04-12 00:30:56 +0800

    chnage title to IKA - International Knowledge Academy

--- src/views/home/default/index.tsx
+++ src/views/home/default/index.tsx
@@ -1,27 +1,10 @@
 // @ts-nocheck
-import Navbar from "components/home/Navbar";
-import Hero from "components/home/Hero";
-import About from "components/home/About";
-import Stats from "components/home/Stats";
-import Programs from "components/home/Programs";
-import TrainingFields from "components/home/TrainingFields";
-import Locations from "components/home/Locations";
-import Features from "components/home/Features";
-import Footer from "components/home/Footer";
+import ComingSoon from "components/home/ComingSoon";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <About />
-      <Stats />
-      <Programs />
-      <TrainingFields />
-      <Locations />
-      <Features />
-      {/* <CTA /> */}
-      <Footer />
+      <ComingSoon/>
     </div>
   );
 };
