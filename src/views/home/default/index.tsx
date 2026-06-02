a648ce2b3e581927df17f0f9172a8489776c0e6b
adnanmadi241 <adnan.madi@codeandcanvas.io>
2026-06-02 13:57:06 +0800

    added components to home page.

--- src/views/home/default/index.tsx
+++ src/views/home/default/index.tsx
@@ -1,10 +1,34 @@
 // @ts-nocheck
-import ComingSoon       from "components/home/ComingSoon";
+import Navbar           from "components/home/Navbar";
+import Hero             from "components/home/Hero";
+import About            from "components/home/About";
+import Stats            from "components/home/Stats";
+import Programs         from "components/home/Programs";
+import UpcomingPrograms from "components/home/UpcomingPrograms";
+import TrainingFields   from "components/home/TrainingFields";
+import HowToEnroll      from "components/home/HowToEnroll";
+import HowToBeTrainer   from "components/home/HowToBeTrainer";
+import Locations        from "components/home/Locations";
+import Features         from "components/home/Features";
+import Services         from "components/home/Services";
+import Footer           from "components/home/Footer";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
-      <ComingSoon/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Stats />
+      <Programs />
+      <TrainingFields />
+      <UpcomingPrograms />
+      <Features />
+      <HowToEnroll />
+      <HowToBeTrainer />
+      <Locations />
+      <Services />
+      <Footer />
     </div>
   );
 };
